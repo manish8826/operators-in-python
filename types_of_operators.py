b1eb40371862935c522a3ce2e1ed0f6898cf6b73
manish8826 <107915498+manish8826@users.noreply.github.com>
2022-07-13 14:54:29 +0530

    Add files via upload

---
+++ types_of_operators.py
@@ -0,0 +1,41 @@
+#  3 types of operators 1. arithmetic 2. comparison 3. logical
+
+
+#arithmetic
+print(1+1) #addition
+print(3-2) #subtraction
+print(4*4) #multiply
+print(8/2) #divide
+print(5%2) #remainder
+print(5//2) #it doesn't give decimal value
+print(5**2) #square,cube...so on
+
+# shorcuts in arithmetic operators
+a = 6
+a = a + 2
+
+a += 2 
+a -= 2
+a *= 2
+a /= 2
+
+print(a)
+
+# comparison operators =,<,> in this case result would be in boolean
+print(3>=2)
+print(3<=2)
+print(3==3)
+print(2!=3)
+
+#logical operators types : 1.or 2.and 3.not
+
+print(3>2 or 6<5) #if one the condition is true
+print(3>2 and 4<3) # ifboth the condition is true
+print( not 3<2) # it revers the condition
+
+
+
+
+
+
+
